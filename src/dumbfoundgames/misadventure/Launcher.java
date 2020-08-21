d2f63119ebe9b2b51c48b8598cb642b431c96f09
FossilFind <33360705+FossilFind@users.noreply.github.com>
2020-08-21 18:03:42 -0500

    Initial commit

---
+++ src/dumbfoundgames/misadventure/Launcher.java
@@ -0,0 +1,11 @@
+package dumbfoundgames.misadventure;
+
+public class Launcher
+{
+	public static final Misadventure MAIN = new Misadventure();
+	
+	public static void main(String[] args)
+	{
+		MAIN.start();
+	}
+}
---
+++ src/dumbfoundgames/misadventure/Misadventure.java
@@ -0,0 +1,28 @@
+package dumbfoundgames.misadventure;
+
+import java.awt.Graphics;
+
+import fossilfind.library.engine.Engine;
+
+/**
+ * The main class
+ * 
+ * @author Zach
+ */
+public class Misadventure extends Engine
+{
+	public Misadventure()
+	{
+		super("Gavin and Zach's Mighty Misadventure");
+	}
+	
+	@Override
+	public void tick()
+	{
+	}
+	
+	@Override
+	public void render(Graphics g)
+	{
+	}
+}
