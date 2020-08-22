1d3c676e87ac914d5c836fb48dc8111533e396c9
FossilFind <33360705+FossilFind@users.noreply.github.com>
2020-08-22 13:17:29 -0500

    Added Inputs

--- src/dumbfoundgames/misadventure/Misadventure.java
+++ src/dumbfoundgames/misadventure/Misadventure.java
@@ -5,18 +5,23 @@ import java.awt.Graphics;
 import dumbfoundgames.misadventure.state.State;
 import dumbfoundgames.misadventure.state.level.Level1State;
 import fossilfind.library.engine.Engine;
+import fossilfind.library.input.InputManager;
 
-/**
- * The main class
- * 
- * @author Zach
- */
 public class Misadventure extends Engine
 {
+	private InputManager inputs;
+	private Level1State level1;
+	
 	public Misadventure()
 	{
 		super("Gavin and Zach's Mighty Misadventure");
-		State.setState(new Level1State());
+		
+		inputs = new InputManager();
+		level1 = new Level1State();
+		
+		window.getFrame().addKeyListener(inputs);
+		
+		State.setState(level1);
 	}
 	
 	@Override
@@ -29,5 +34,15 @@ public class Misadventure extends Engine
 	public void render(Graphics g)
 	{
 		State.getState().render(g);
+	}
+	
+	public Level1State getLevel1()
+	{
+		return level1;
+	}
+	
+	public InputManager getInputs()
+	{
+		return inputs;
 	}
 }
